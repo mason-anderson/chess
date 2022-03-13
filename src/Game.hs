9bfb657bf2d750e21948c478c250bcd2b4e1c72d
Mason Anderson <masonanderson0@gmail.com>
2022-03-12 21:16:38 -0700

    you must now escape check with your move

--- src/Game.hs
+++ src/Game.hs
@@ -3,6 +3,7 @@ module Game where
 import Data.Array
 import Data.List
 import Data.Bifunctor
+import Data.Maybe
 
 data PColor = White | Black
     deriving (Eq, Show)
@@ -47,11 +48,25 @@ initialBoard = listArray ((0,0),(7,7)) $ concat $ transpose
 checkMate :: Board -> Maybe PColor
 checkMate b = undefined
 
-legalMove :: Pos -> Pos -> (Pos,Pos) -> Board -> Bool
-legalMove iPos fPos lastMove board = movePossible iPos fPos board
+makeMove :: Pos -> Pos -> Board -> Board
+makeMove iPos fPos board = board // [(iPos, SEmpty), (fPos, board ! iPos)]
 
-inCheck :: Board -> PColor
-inCheck b = undefined
+legalMove :: Pos -> Pos -> (Pos,Pos) -> Board -> Bool
+legalMove iPos fPos lastMove board = movePossible iPos fPos board && not (inCheck color (makeMove iPos fPos board))
+    where
+        color = fromJust $ getColor (board ! iPos)
+
+-- return True if that color's king is in check
+-- we do this by checking to see if the king's pos is a legal move for any pieces
+inCheck :: PColor -> Board -> Bool
+inCheck color board = any (\p -> movePossible p kingPos board) oppositeColorPiecesPos
+    where
+        kingPos = fst $ head $ filterKing $ assocs board
+        filterKing = filter (\case (_,SPiece King c) -> c == color; _ -> False)
+
+        oppositeColorPiecesPos = map fst $ filterOppColor $ assocs board
+        filterOppColor = filter (\case (_, SPiece _ c) -> c /= color; _ -> False)
+
 
 -- checks if a move is possible (not including moving in check)
 movePossible :: Pos -> Pos -> Board -> Bool
@@ -61,8 +76,12 @@ movePossible i f b = f `elem` legalSquares
         -- return a list of legal squares for the piece at i
         legalSquares :: [Pos]
         legalSquares = case piece of
-            (SPiece Pawn c) -> filter (legalTake c) [bimap (+1) (dir 1) i, bimap (subtract 1) (dir 1) i] ++ [x | x <- [second (dir 1) i, second (dir 2) i], onBoard x && b ! x == SEmpty]
+            (SPiece Pawn c) -> filter (legalTake c) [ bimap (+1) (dir 1) i, bimap (subtract 1) (dir 1) i] -- take on diagonals
+                            ++ moveOne -- move 1
+                            ++ [x | x<-[second (dir 2) i], not (null moveOne) && b ! x == SEmpty && (snd i == 1 || snd i == 6)] -- move 2 (first move only)
+                -- dir returns the direction of movement for the correct color
                 where dir x = if c == White then (+x) else subtract x
+                      moveOne = [x | x<-[second (dir 1) i], onBoard x && b ! x == SEmpty]
                       legalTake c x = onBoard x && (case b ! x of
                                 SEmpty -> False
                                 (SPiece _ c2) -> c2 /= c
